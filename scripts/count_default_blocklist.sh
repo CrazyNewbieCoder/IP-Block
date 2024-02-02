2f645c4e013c6dc7fabd3ce2eae4e319b4e0db81
strelok <git@pcsupport.com.ua>
2024-02-02 22:09:07 +0200

    Исправил комментарии

--- scripts/count_default_blocklist.sh
+++ scripts/count_default_blocklist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# Считает, сколько доменов в файле default_blocklist.txt
+# Підраховує кількість доменів у файлі default_blocklist.txt
 
 sed -e '/#/d' -e '/^$/d' -e '/[[:space:]]/d' ./sources/default_blocklist.txt | uniq | wc -l
--- scripts/count_default_whitelist.sh
+++ scripts/count_default_whitelist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# Считает, сколько доменов в файле default_whitelist.txt
+# Підраховує кількість доменів у файлі default_whitelist.txt
 
 sed -e '/#/d' -e '/^$/d' -e '/[[:space:]]/d' ./sources/default_whitelist.txt | uniq | wc -l
